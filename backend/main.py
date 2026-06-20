c9ad7d838919402ed2193d41b7e3a99a6f67f860
Dharshini S <dharshinis0610@outlook.com>
2026-06-20 12:20:11 +0530

    1.20 hrs done

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from core.config import settings
+from routers import story, job
 app = FastAPI(
     title="Choose Your Own Adventure API",
     descriptions="api to generate ccol stories",
@@ -16,7 +17,10 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+) 
+
+app.include_router(story.router, prefix=settings.API_PREFIX)
+app.include_router(job.router,prefix=settings.API_PREFIX)
 
 if __name__ == "__main__":
     import uvicorn
